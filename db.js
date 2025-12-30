fc3f215177f7370536dba0babe8aa134361f74df
Dianased <dianavslvna@gmail.com>
2025-12-31 00:02:43 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,9 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-export default pool;
